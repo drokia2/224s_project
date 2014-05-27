9f1f12c5115703a0467a7f87c50ec8d9e9debbbf
Sam Beder <sambeder@rescomp-13-274678.stanford.edu>
2014-05-26 23:17:20 -0700

    added csv converter

--- mlf_to_csv.py
+++ mlf_to_csv.py
@@ -4,6 +4,7 @@
 
 from subprocess import call
 import csv
+import re
 
 natives = open('data/FWOA.txt').readlines()
 natives = [x.strip() for x in natives]
@@ -13,15 +14,36 @@ foreigners = [x.strip() for x in foreigners]
 NUM_TRANSCRIPTS = 4
 
 for i in range(NUM_TRANSCRIPTS):
-	csv_filename = 'data/csvs/transcript_%d.csv' % i+1
+	csv_filename = 'data/csvs/transcript_%d.csv' % (i+1)
 	csv_file = open(csv_filename, 'wb')
-	csv_writer = csv.writer(csv_file, delimeter = ',')
+	csv_writer = csv.writer(csv_file,delimiter = ',')
 
 	for native in natives:
+		csv_writer.writerow('')
+
+		row_string = ['person', '%s' % native]
+
 		filename = 'data/mlfs/native/%s_%d.mlf' % (native, i+1)
 		mlf_data = open(filename).readlines()
 
-		for 
+		pattern = re.compile('\d*\s\d*\s(.*)\s(\d*\.\d*)\s?(\w*)')
+
+		for row in mlf_data:
+			m = pattern.match(row)
+			if m:
+				if m.group(3):
+					csv_writer.writerow(row_string)
+					row_string = []
+					row_string.append(m.group(3))
+
+				row_string.append(m.group(1))
+				row_string.append(m.group(2))
+
+		csv_writer.writerow(row_string)
+
+
+
+
 
 
 

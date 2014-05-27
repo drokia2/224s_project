6928ba824042173ef83fdf0b6c2a5d5f16a3d116
drokia2 <drokia2@gmail.com>
2014-05-26 20:41:13 -0700

    added josh megan and sean-f

--- align_all.py
+++ align_all.py
@@ -8,4 +8,28 @@
 #   python align_all.py name+           # to run alignment on one or more specific names listed; 
                                         # names are delimeted by spaces i.e. python align_all.py adriana sam ankit
 
+# you might have to do sudo right before running this
+# make sure there is a file called "TEXT_GRID.TextGrid" in the current directory
 
+from subprocess import call
+
+natives = open('data/FWOA.txt').readlines()
+natives = [x.strip() for x in natives]
+foreigners = open('data/FWA.txt').readlines()
+foreigners = [x.strip() for x in foreigners]
+print natives
+print foreigners
+
+NUM_TRANSCRIPTS = 4
+TRANSCRIPTS = ['transcript_1.txt', 'transcript_2.txt', 'transcript_3.txt', 'transcript_4.txt']
+TEXT_GRID = "TEXT_GRID.TextGrid"
+
+for native in natives:
+  for i in range(NUM_TRANSCRIPTS):
+    transcript = './data/transcripts/%s' % TRANSCRIPTS[i]
+    wav_file = './data/sounds/%s_%d.wav' % (native, i+1)
+    failure = call(['python', 'align.py', wav_file, transcript, TEXT_GRID ])
+    if (failure):
+      print "ERROR WITH ", native , i+1
+      print "\n"
+    

fde1e54fe02d88977c1c7182a6e6bedc8c44fe49
drokia2 <drokia2@gmail.com>
2014-05-26 17:46:48 -0700

    added align_all.py description

---
+++ align_all.py
@@ -0,0 +1,11 @@
+#align_all.py  
+#runs align all all of the sound file with there corresponding transcripts
+#and dumps the files in data/mlfs/ directory
+#The mlf files have the same naming scheme as the soudn files i.e. 'adriana_1.mlf'
+#
+#usage:
+#   python align_all.py                 # to run alignment on all students
+#   python align_all.py name+           # to run alignment on one or more specific names listed; 
+                                        # names are delimeted by spaces i.e. python align_all.py adriana sam ankit
+
+
